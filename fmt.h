237c99eaf454123e19c38e10beca33c37c1c2249
0x1fe1 <pangolecimal@gmail.com>
2024-09-04 23:42:50 +0300

    progress

--- fmt.h
+++ fmt.h
@@ -3,11 +3,25 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-extern void fmt(const char* format, ...);
+
+extern int fmt(const char* format, ...);
+extern int ffmt(FILE* output, const char* format, ...);
 
 #ifdef FMT_IMPL
 
-void fmt(const char* str, ...) {
+int fmt(const char* format, ...) {
+    int result;
+    va_list args;
+
+    va_start(args, format);
+    result = ffmt(stdout, format, args);
+    va_end(args);
+
+    return result;
+}
+
+int ffmt(FILE* output, const char* str, ...) {
+    int result = 0;
     va_list ptr;
     va_start(ptr, str);
 
@@ -22,79 +36,86 @@ void fmt(const char* str, ...) {
             token[k] = '\0';
             k = 0;
             if (token[0] != '%') {
-                fprintf(stdout, "%s", token);
-            } else {
-                int j = 1;
-                char ch1 = 0;
+                fprintf(output, "%s", token);
+                continue;
+            }
 
-                // this loop is required when printing formatted value like 0.2f, when ch1='f' loop ends
-                while ((ch1 = token[j++]) < '9') { }
+            int j = 1;
+            char ch1 = 0;
 
-                switch (ch1) {
-                    // for integers
-                    case 'i':
-                    case 'd':
-                    case 'u':
-                    case 'h':
-                        fprintf(stdout, token, va_arg(ptr, int));
-                        break;
+            // this loop is required when printing formatted value like 0.2f, when ch1='f' loop ends
+            while ((ch1 = token[j++]) < '9') { }
 
-                    // for characters
-                    case 'c':
-                        fprintf(stdout, token, va_arg(ptr, int));
-                        break;
+            switch (ch1) {
+            case 'i':
+            case 'd':
+            case 'u':
+            case 'h':
+                fprintf(output, token, va_arg(ptr, int));
+                break;
 
-                    case 'f':
-                        // for float values
-                        fprintf(stdout, token, va_arg(ptr, double));
-                        break;
+            case 'c':
+                fprintf(output, token, va_arg(ptr, int));
+                break;
 
-                    case 'l':
-                        char ch2 = token[2];
+            case 'f':
+                fprintf(output, token, va_arg(ptr, double));
+                break;
 
-                        // for long int
-                        if (ch2 == 'u' || ch2 == 'd' || ch2 == 'i') {
-                            fprintf(stdout, token, va_arg(ptr, long));
-                        }
-
-                        // for double
-                        else if (ch2 == 'f') {
-                            fprintf(stdout, token, va_arg(ptr, double));
-                        }
-                        break;
-
-                    case 'L':
-                        char ch2 = token[2];
-
-                        // for long long int
-                        if (ch2 == 'u' || ch2 == 'd' || ch2 == 'i') {
-                            fprintf(stdout, token, va_arg(ptr, long long));
-                        }
-
-                        // for long double
-                        else if (ch2 == 'f') {
-                            fprintf(stdout, token, va_arg(ptr, long double));
-                        }
-                        break;
-
-                    // for strings
-                    case 's':
-                        fprintf(stdout, token, va_arg(ptr, char*));
-                        break;
-
-                    // print the whole token
-                    // if no case is matched
-                    default:
-                        fprintf(stderr, "Unrecognized token: [%s]\n", token);
-                        fprintf(stdout, "%s", token);
-                        break;
+            case 'l':
+            case 'L':
+                char ch2 = token[2];
+                printf("\n\nDEBUG: %s\n\n", token);
+                if (ch2 == 'u' || ch2 == 'd' || ch2 == 'i') {
+                    if (ch1 == 'l') {
+                        fprintf(output, token, va_arg(ptr, long));
+                    } else {
+                        fprintf(output, token, va_arg(ptr, long long));
+                    }
                 }
+                else if (ch2 == 'f') {
+                    if (ch1 == 'l') {
+                        fprintf(output, token, va_arg(ptr, double));
+                    } else {
+                        fprintf(output, token, va_arg(ptr, long double));
+                    }
+                }
+                break;
+
+            case 's':
+                fprintf(output, token, va_arg(ptr, char*));
+                break;
+
+            default:
+                fprintf(stderr, "ERROR: Unrecognized format: [%s]\n", token);
+                fprintf(output, "%s", token);
+                result = 1;
+                break;
             }
         }
     }
 
     va_end(ptr);
+    return result;
 }
 
 #endif // FMT_IMPL
 #endif // FMT_HEADER
+
+/*
+
+from https://alvinalexander.com/programming/printf-format-cheat-sheet
+
+%c  character
+%d  decimal (integer) number (base 10)
+%e  exponential floating-point number
+%f  floating-point number
+%i  integer (base 10)
+%o  octal number (base 8)
+%s  a string of characters
+%u  unsigned decimal (integer) number
+%x  number in hexadecimal (base 16)
+%%  print a percent sign
+\%  print a percent sign
+
+*/
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "fmt.h"
 
 int main() {
-    fmt("%s %d %.5d\n", "World", 34+35, 21 * 20);
+    long foo = 1;
+    printf("%s %d %.5d %lu\n", "World", 34+35, 21 * 20, foo);
+    ffmt(stdout, "%s %d %.5d %lu\n", "World", 34+35, 21 * 20, foo);
     return 0;
 }
